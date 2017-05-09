7293e9be41c8b72f6bba52252847add99ad8e62a
TodMcChesney <codescribe.net@gmail.com>
2017-05-09 09:50:09 -0400

    feat: Add locations data model

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,45 @@
 // Global variables
 var map;
 
+// Locations data model
+var model = [
+    {
+        title: 'Sierra Nevada Brewery',
+        location: {
+            lat: 35.430382,
+            lng: -82.548919
+        }
+    },
+    {
+        title: 'New Belgium Brewery',
+        location: {
+            lat: 35.586875,
+            lng: -82.572707
+        }
+    },
+    {
+        title: 'Wicked Weed Brewing Pub',
+        location: {
+            lat: 35.591683,
+            lng: -82.551126
+        }
+    },
+    {
+        title: 'Burial Beer Company',
+        location: {
+            lat: 35.588049,
+            lng: -82.553796
+        }
+    },
+    {
+        title: 'Green Man Brewery',
+        location: {
+            lat: 35.588720,
+            lng: -82.553007
+        }
+    }
+];
+
 // Google Maps API script callback function that initializes the map
 function initMap() {
     'use strict';
