423b0afb2df42943b99389bdd1a5f81154362d94
TodMcChesney <codescribe.net@gmail.com>
2017-06-23 17:12:16 +0200

    feat: Add build task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,13 @@
 
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
+const rm = require('gulp-rm');
+const cssnano = require('gulp-cssnano');
+const rename = require('gulp-rename');
+const processhtml = require('gulp-processhtml');
+const htmlmin = require('gulp-htmlmin');
+const uglify = require('gulp-uglify');
+
 
 // Running dev tasks from the CLI:
 // $ gulp (Runs browserSync and watches for changes)
@@ -23,3 +30,55 @@ gulp.task('default', ['browserSync'], () => {
     gulp.watch('src/js/*.js', browserSync.reload);
     gulp.watch('src/index.html', browserSync.reload);
 });
+
+// Run build task from the CLI:
+// $ gulp build (Runs all production tasks in sequence to build dist folder)
+
+// clean:dist
+gulp.task('clean:dist', () =>
+    gulp.src('dist/**/*', {
+        read: false
+    })
+    .pipe(rm())
+);
+
+// minify:js
+gulp.task('minify:js', ['clean:dist'], () =>
+gulp.src('src/js/*.js')
+.pipe(uglify())
+.pipe(rename({
+    suffix: '.min'
+}))
+.pipe(gulp.dest('dist/js'))
+);
+
+// minify:css
+gulp.task('minify:css', ['clean:dist'], () =>
+    gulp.src('src/css/styles.css')
+    .pipe(cssnano())
+    .pipe(rename({
+        suffix: '.min'
+    }))
+    .pipe(gulp.dest('dist/css'))
+);
+
+// minify:html
+gulp.task('minify:html', ['clean:dist'], () =>
+    gulp.src('src/*.html')
+    .pipe(processhtml())
+    .pipe(htmlmin({
+        collapseWhitespace: true
+    }))
+    .pipe(gulp.dest('dist'))
+);
+
+// copy
+gulp.task('copy', ['clean:dist'], () =>
+    gulp.src(['src/js/lib/*.js', 'src/img/**/*.+(png|jpg|svg)', 'src/*.!(html)'], {
+        base: 'src'
+    })
+    .pipe(gulp.dest('dist'))
+);
+
+// build
+gulp.task('build', ['clean:dist', 'minify:js', 'minify:css', 'minify:html', 'copy']);
